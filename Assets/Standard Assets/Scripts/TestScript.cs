501f8cfd9d8f2bdf635bb9e43c0f1a07c4652025
acpirate <3apx3zss29@snkmail.com>
2014-08-19 10:50:52 -0400

    added temporary object and code to test committing
    added a temporary object and script to test making changes to the
    project

---
+++ Assets/Standard Assets/Scripts/TestScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestScript : MonoBehaviour {
+
+	public GUIStyle tempStyle;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	//2014-08aug-19 ACP adding gui event to draw title
+	void OnGUI() {
+		GUI.Label(new Rect(100,100,1000,100),"<color=orange>TOTEM QUEST</color>",tempStyle);
+	}
+
+
+
+}
