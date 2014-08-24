da3bd921d904c355f8ff5351246b2df3a3ad473e
Ylla- <sangheili_halo@hotmail.com>
2014-08-24 14:32:19 -0500

    Added dash
    Also reorganized the Controller code, and fixed the jump horizont, now
    you can jump when more than half the collider is not on the ground.

--- Assets/Standard Assets/Scripts/Controller.cs
+++ Assets/Standard Assets/Scripts/Controller.cs
@@ -2,42 +2,78 @@
 using System.Collections;
 
 public class Controller : MonoBehaviour {
+	//for horizontal movement
 	public float maxSpeed = 5f;
-	public float HorizontalForceonAir = 8f;
-	
-	public float JumpDuration = 0.150f;
-	public float JumpPressedTime;
-	public bool JumpingPressed;
-	
-	public bool facingRight = true;
 	public float move;
+	bool moveAllowed;
+	//public float HorizontalForceonAir = 8f;
+
+	//for non fixed jump
+	public float JumpDuration = 0.150f;
+	float JumpPressedTime, JumpingPressed;
+	public float jumpForce = 50f;
+	//for fixed jump
+	public float soloJumpForce = 440;
+
+
+	//for animation
+	public bool facingRight = true;
 	public Animator anim;
 
-	
+	//for dash
+	public float dashDuration = 0.1f, dashSpeed = 20f;
+	float dashStartTime; bool dash;
+
+
+	// ground check
 	public bool onGround = false;
-	public Transform groundCheck;
+	public Transform groundCheckL, groundCheckR;
 	float groundRadius = 0.1f;
 	public LayerMask theGround;
-	public float jumpForce = 50f;
-	public float soloJumpForce = 440;
 
 
 	
 	void Start () {
 		anim = GetComponent<Animator> ();
+		moveAllowed = true;
 	}
 	
 	
 	void FixedUpdate () {
 		//on ground check
-		onGround = Physics2D.OverlapCircle(groundCheck.position, groundRadius, theGround);
-		
-		//moving in the x axis
+		if (Physics2D.OverlapCircle (groundCheckL.position, groundRadius, theGround) == true) {
+			onGround = true;
+				}
+		else {
+			onGround = Physics2D.OverlapCircle(groundCheckR.position, groundRadius, theGround);
+		}
+
+		// Dash rabbit
+		if (dash){
+			Debug.Log ("Dash");
+			dash = false;
+			moveAllowed = false;
+			dashStartTime = Time.time;            //if and else just to dash in the right direction
+			if (facingRight)rigidbody2D.velocity = new Vector2 (dashSpeed, rigidbody2D.velocity.y); 
+			else rigidbody2D.velocity = new Vector2 (-dashSpeed, rigidbody2D.velocity.y);
+		}
+		if ((Time.time - dashStartTime) < dashDuration) {
+			if (facingRight)rigidbody2D.velocity = new Vector2 (dashSpeed, rigidbody2D.velocity.y);
+			else rigidbody2D.velocity = new Vector2 (-dashSpeed, rigidbody2D.velocity.y);
+		} else {
+			rigidbody2D.velocity = new Vector2 (move*maxSpeed, rigidbody2D.velocity.y);
+			moveAllowed = true;
+		}
+
+
+		//moving in the x axis, moveAllowed allows to take away the movement control from the player
+		if(moveAllowed){
 		move = Input.GetAxis ("Horizontal");
 		//if(onGround)
 			rigidbody2D.velocity = new Vector2 (move * maxSpeed, rigidbody2D.velocity.y);
 		//if(!onGround)
 		//	rigidbody2D.AddForce(new Vector2 (move * HorizontalForceonAir, 0));
+	    }
 		
 		
 		/*jumping hold to go higher
@@ -75,14 +111,14 @@ public class Controller : MonoBehaviour {
 	
 	void Update(){
 	//Fixed jump
-	if (Input.GetButtonDown ("Jump")) {
-						if (onGround) {
-								rigidbody2D.AddForce (new Vector2 (0, soloJumpForce));
-						}
-				}
+	if (Input.GetButtonDown("Jump") && onGround){
+		rigidbody2D.AddForce (new Vector2 (0, soloJumpForce));
+		}
 
-		
-		
+		// it seems you cant use GetButtonDown in the FixedUpdate()
+	if (Input.GetButtonDown ("Dash") && onGround) {
+						dash = true;
+				}
 		/*animator variables
 		anim.SetFloat ("VerticalVelocity", rigidbody2D.velocity.y);
 		anim.SetBool ("Jump", JumpingPressed);
