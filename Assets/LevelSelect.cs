cbec67ddaaed1dad98abcce1346279dcfe5a1337
acpirate <3apx3zss29@snkmail.com>
2014-08-28 14:15:01 -0400

    Added stub scenes
    stub scenes for intro, title, level select are added
    intro, title, and levelselect are linked, other scenes aren’t

---
+++ Assets/LevelSelect.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelSelect : MonoBehaviour {
+
+	public string ForestLevel;
+	public string PlainsLevel;
+	public string CavesLevel;
+	public string FinalLevel;
+	public string Title;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// 2014-08AUG-28 ACP Buttons properties are hardcoded should be re-written to be easier to edit
+	void OnGUI () {
+		//plains
+		if 
+			(GUI.Button(new Rect(10,10,100,100),"Plains")) Application.LoadLevel(PlainsLevel);
+		//forest
+		if 
+			(GUI.Button(new Rect(10,120,100,100),"Forest")) Application.LoadLevel(ForestLevel);
+		//caves
+		if 
+			(GUI.Button(new Rect(10,230,100,100),"Caves")) Application.LoadLevel(CavesLevel);
+
+		//final level starts disabled
+		GUI.enabled=false;
+		if 
+			(GUI.Button(new Rect(10,340,100,100),"Final")) Application.LoadLevel(FinalLevel);
+		GUI.enabled=true;
+
+		//back to title
+		if
+			(GUI.Button(new Rect(Screen.width-100-10,10,100,100),"Back To Title")) Application.LoadLevel(Title);
+	}
+}
---
+++ Assets/SpaceForNextScene.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpaceForNextScene : MonoBehaviour {
+
+	public string nextScene;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.Space)) 
+			Application.LoadLevel(nextScene);
+	}
+
+
+}
