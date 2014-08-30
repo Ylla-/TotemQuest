e23af019e4e04384f5a6cd494fe0cfdb10814168
sprawls <alex3521@hotmail.fr>
2014-08-29 20:56:23 -0400

    Implemented Basis of Energy Orbs, need to tune the mouvements

---
+++ Assets/Standard Assets/Scripts/UI_Scripts/EnergyBar.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EnergyBar : MonoBehaviour {
+	/// <summary>
+	/// This Script manages the List of orbs that are following the player. Work In Progress !
+	/// </summary>
+	/// 
+	public Transform target; //Target the sprites are gonna follow
+	public GameObject energySprite; // Prefab to be instantiated as orbs
+
+	public int maxEnergy = 10; //link these to the Player's energy value later
+	public int currentEnergy = 0;
+	public List<EnergyOrb> orbList;
+
+	void Awake () {
+		orbList = new List<EnergyOrb>();
+	}
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Controls for testing purposes only :
+		if(Input.GetKeyDown (KeyCode.Z)){
+			Debug.Log ("Test Purpose : Added Orb (Key Z)");
+			CreateOrb();
+		}
+		if(Input.GetKeyDown (KeyCode.X)){
+			Debug.Log ("Test Purpose : Removed Orb (Key X)");
+			RemoveOrb();
+		}
+	}
+
+
+
+	public void UpdateList(){
+		for(int i=0; i<orbList.Count; i++) {
+			orbList[i].orbIndex = i;
+		}
+	}
+
+
+	public void CreateOrb() {
+		if(orbList.Count < maxEnergy) {
+			orbList.Add ((Instantiate(energySprite, target.position, Quaternion.identity) as GameObject).GetComponent<EnergyOrb>()); //Create Orb & add to List
+			orbList[orbList.Count-1].target = target; // Set Orb's Target
+			UpdateList();
+			currentEnergy++;
+		}
+	}
+
+	public void RemoveOrb() {
+		if(orbList.Count > 0) {
+			GameObject orbToDestroy = orbList[orbList.Count-1].gameObject;
+			orbList.RemoveAt (orbList.Count-1); //Note : Once orbs work, try changing this for RemoveAt(0) for cool effect.
+			Destroy (orbToDestroy); //Here, it would be possible to add an animation on destroy.
+			UpdateList();
+			currentEnergy--;
+		}
+	}
+
+}
---
+++ Assets/Standard Assets/Scripts/UI_Scripts/EnergyOrb.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnergyOrb : MonoBehaviour {
+
+	/// <summary>
+	/// This Script dictates the follow behaviour of the orbs. Work In Progress !
+	/// </summary>
+	/// 
+	public Transform target;
+	public int orbIndex; //index in OrbList of EnergyBar.cs
+
+	public float maximumSpeedRange = 1f; //Distance at which orb speed is at his maximum
+	public float speed; //current Speed
+	public bool playerFacingLeft = true;
+
+	private Vector3 direction; //Current Direction
+	private Vector3 orbToModelStartDistance; //Distance between the orb and the player
+	private Vector3 orbToModelAdditionnalDistance; //Additionnal distance added for each orb
+
+	void Start () {
+		if(playerFacingLeft) {
+			orbToModelStartDistance = new Vector3 (-0.2f, 0.15f, 0);
+			orbToModelAdditionnalDistance = new Vector3( -0.1f, Random.Range (0f,0.1f),0);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		direction = SetOrbDirection (); //Get Direction 
+		speed = SetOrbSpeed (); //Get Speed
+
+		transform.Translate(direction*speed*Time.deltaTime);  //Move
+	}
+
+	Vector3 SetOrbDirection() { //Find Direction to go in
+		Vector3 targetPosition = target.position; //Target's position
+		if(playerFacingLeft) {
+			targetPosition += new Vector3 (orbToModelStartDistance.x,orbToModelStartDistance.y,0); //Basic Target Position
+			targetPosition += new Vector3 (orbToModelAdditionnalDistance.x * orbIndex, orbToModelAdditionnalDistance.y, 0);// Target Position based on current Index
+		} else {
+			targetPosition += new Vector3 (-orbToModelStartDistance.x,orbToModelStartDistance.y,0); //Basic Target Position
+			targetPosition += new Vector3 (-orbToModelAdditionnalDistance.x * orbIndex, orbToModelAdditionnalDistance.y, 0);// Target Position based on current Index
+		}
+
+		Vector3 newDirection = (targetPosition - transform.position).normalized;
+		return newDirection;
+	}
+
+	float SetOrbSpeed() {
+		float targetRange = 0.08f * (orbIndex+1); //Set Different ranges for every orb.
+		Vector3 range = transform.position - target.position;
+		range -= (targetRange * range.normalized);
+
+		float speedStep = Mathf.Clamp ((range.magnitude / maximumSpeedRange),0,1);
+		float newSpeed = Mathf.Lerp (0.1f, 35f, speedStep);
+
+		if(range.magnitude > targetRange) {
+			return newSpeed;
+		} else {
+			return 0f;
+		}
+
+
+	}
+
+}
