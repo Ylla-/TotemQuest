74337ec192b2e245bb664a841482f8e9c00ac8f2
sprawls <alex3521@hotmail.fr>
2014-09-12 19:32:42 -0400

    Fixed moving platforms from not moving after death.

--- Assets/Standard Assets/Scripts/MovingPlatform.cs
+++ Assets/Standard Assets/Scripts/MovingPlatform.cs
@@ -1,10 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MovingPlatform : MonoBehaviour {
+public class MovingPlatform : MonoBehaviour {	
+
 	public Transform thisTransform;
 	Rigidbody2D thisRigidbody2D;
 	public float interval = 1f;
+	public float delay = 0f; //Platforms wait "delay" seconds before moving.
 	public float limitX1, limitX2, limitY1, limitY2;
 	float distanceX, speedX, speedY, distanceY;
 	public bool moving, left;
@@ -20,58 +22,69 @@ public class MovingPlatform : MonoBehaviour {
 		speedX = distanceX / interval;
 		distanceY = Mathf.Abs (limitY2 - limitY1);
 		speedY = distanceY / interval;
+
+		StartCoroutine (StartMoving()); // New Coroutine Added To simulate what the previous code was doing.
 	}
-	
+
+
+
 	// Update is called once per frame
 	void FixedUpdate () {
-				if ((Time.time / interval) == 0) {
-						moving = true;
-				}
-
 		
-				if (moving) { 
-			
-						if (!left) {
-								if (limitX2 >= rigidbody2D.position.x && limitY2 >= rigidbody2D.position.y) {
-										rigidbody2D.velocity = new Vector2 (speedX, rigidbody2D.velocity.y);
-										rigidbody2D.velocity = new Vector2 (rigidbody2D.velocity.x, +speedY);
-								} else if (!left) {
-										rigidbody2D.velocity = new Vector2 (0, 0);
-										left = true;
-								}
-						}
-							
-						if (left) {
-							if (limitX1 <= rigidbody2D.position.x && limitY1 <= rigidbody2D.position.y) { 
-										rigidbody2D.velocity = new Vector2 (-speedX, rigidbody2D.velocity.y);
-										rigidbody2D.velocity = new Vector2 (rigidbody2D.velocity.x, -speedY);
-								} else if (left) {
-										rigidbody2D.velocity = new Vector2 (0, 0);
-										left = false;
-								}
-						}
-
-						
-				}
-
+		/* // @Ylla : 
+		 * // I commented this out since this causes the platforms not to move when respawning since time is never equal to 0 again. 
+		 * // Also, Why is moving not always true ? Is it to have delayed Platforms ? If so I've replaced it with a Coroutine that does
+		 * // The same thing and works on respawn. 
+		if ((Time.time / interval) == 0) {
+			moving = true;
 		}
+		*/
+		
+		if (moving) { 
+			if (!left) {
+				if (limitX2 >= rigidbody2D.position.x && limitY2 >= rigidbody2D.position.y) {
+					rigidbody2D.velocity = new Vector2 (speedX, rigidbody2D.velocity.y);
+					rigidbody2D.velocity = new Vector2 (rigidbody2D.velocity.x, +speedY);
+				} else if (!left) {
+					rigidbody2D.velocity = new Vector2 (0, 0);
+						left = true;
+				}
+			}				
+			if (left) {
+				if (limitX1 <= rigidbody2D.position.x && limitY1 <= rigidbody2D.position.y) { 
+					rigidbody2D.velocity = new Vector2 (-speedX, rigidbody2D.velocity.y);
+					rigidbody2D.velocity = new Vector2 (rigidbody2D.velocity.x, -speedY);
+				} else if (left) {
+					rigidbody2D.velocity = new Vector2 (0, 0);
+					left = false;
+				}
+			}
+		}
+
+	}
 
 
 
 	void OnCollisionEnter2D(Collision2D coll) {
 		if (coll.collider is CircleCollider2D) {
-						Debug.Log ("on platform");
-						Controller c = (Controller)coll.gameObject.GetComponent ("Controller");	
-						c.movingRigidbody2D = thisRigidbody2D;
-						c.onMovingPlatform = true;
-				}
-		}
+			Debug.Log ("on platform");
+			Controller c = (Controller)coll.gameObject.GetComponent ("Controller");	
+			c.movingRigidbody2D = thisRigidbody2D;
+			c.onMovingPlatform = true;
+			}
+	}
 	void OnCollisionExit2D(Collision2D coll){
 		if (coll.collider is CircleCollider2D) {
-						Debug.Log ("off platform");
-						Controller c = (Controller)coll.gameObject.GetComponent ("Controller");	
-						c.onMovingPlatform = false;
-				}
+		Debug.Log ("off platform");
+		Controller c = (Controller)coll.gameObject.GetComponent ("Controller");	
+		c.onMovingPlatform = false;
+		}
+	}
+
+	//This coroutine wait for interval seconds then sets the moving bool to true
+	IEnumerator StartMoving(){
+		yield return new WaitForSeconds(delay);
+		moving = true;
 	}
 
 	
