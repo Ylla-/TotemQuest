f7c6436b8e8d526f2bafb75d50147db7c7733a46
Alex <alex3521@hotmail.fr>
2014-09-16 15:14:41 -0400

    Added A basic scrolling background

---
+++ Assets/Standard Assets/Scripts/BackgroundScrolling.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundScrolling : MonoBehaviour {
+
+	public float scrollSpeed = 0.2f;
+	public float maxScrollX = 1f; //Maximum scrolling of the background on X Axis
+	public float maxScrollY = 1f; //Maximum scrolling of the background on Y Axis.
+	public float mouvementOfPlayerX = 100f;
+	public float mouvementOfPlayerY = 100f;
+
+	private Transform player;
+	private Vector3 startPosition;
+	private float currentXMouvement;
+	private float currentYMouvement;
+
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player").transform;
+		startPosition = player.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		SetNewMouvement();
+		transform.localPosition = new Vector3(maxScrollX * currentXMouvement, maxScrollY + currentYMouvement,100);
+	}
+
+	void SetNewMouvement() {
+		//X Mouvement
+		currentXMouvement = (startPosition.x - player.position.x);
+		if(Mathf.Abs(currentXMouvement) > mouvementOfPlayerX) currentXMouvement = mouvementOfPlayerX;
+		currentXMouvement = currentXMouvement / mouvementOfPlayerX;
+		//Y mouvement
+		currentYMouvement = (startPosition.y - player.position.y);
+		if(Mathf.Abs(currentYMouvement) > mouvementOfPlayerY) currentYMouvement = mouvementOfPlayerY;
+		currentYMouvement = currentYMouvement / mouvementOfPlayerY;
+	}
+}
