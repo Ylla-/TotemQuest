2a9424a5a63afe3b6ce42b43a9bd06b55f716f5a
sprawls <alex3521@hotmail.fr>
2014-09-21 22:14:26 -0400

    Added Mouvement between Levels and lvlSelect
    -Added Doors that end level and registers that in "GameManager.cs"
    -Added Placeholder scenes for plains and Caves Level.
    
    Tomorrow, I will add the final level placeholder and make it so it unlocks
    when all three levels are completed.

---
+++ Assets/Standard Assets/Scripts/CompleteLevel.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CompleteLevel : MonoBehaviour {
+
+	public int levelToComplete = 0;
+	GameManager manager;
+	GameObject player;
+	// Use this for initialization
+	void Start () {
+		manager = GameObject.Find ("GameManager").GetComponent<GameManager>();
+		player = GameObject.FindGameObjectWithTag ("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown("Vertical")||Input.GetAxis ("Vertical")>0.25) {
+			if(gameObject.renderer.bounds.Intersects(player.renderer.bounds)){
+				FinishLevel();
+			} 
+		}
+	}
+
+	void FinishLevel(){
+		manager.levelFinished [levelToComplete] = true;
+		Application.LoadLevel (2);
+	}
+
+}
---
+++ Assets/Standard Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	static bool isLoaded = false;
+	public bool[] levelFinished;
+
+	void Awake(){
+		if(isLoaded == false) {
+			GameObject.DontDestroyOnLoad (gameObject);
+			isLoaded = true;
+			gameObject.name = "GameManager";
+		} else {
+			Destroy (gameObject);
+		}
+	}
+
+	void Start () {
+		levelFinished = new bool[3];
+		levelFinished [0] = false;
+		levelFinished [1] = false;
+		levelFinished [2] = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
