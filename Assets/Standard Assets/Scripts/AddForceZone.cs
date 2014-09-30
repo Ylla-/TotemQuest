0bb6c48c019decb4be5281c72f5b2dd153d417cb
Alex <alex3521@hotmail.fr>
2014-09-30 15:45:24 -0400

    Added Wind in Forest level
    -Added Windzones Objects to the game. These objects add a force on the
    player when they collide with it.
    -Windozes are located in Enemies/Environemental
    
    -Also fixed all doors locations. It seems Devieus's update to the door
    reset all doors to their original prefab.

---
+++ Assets/Standard Assets/Scripts/AddForceZone.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// This script makes the player currently colliding with this object's collider to be affected by a force in a certain direction.
+/// </summary>
+
+
+public class AddForceZone : MonoBehaviour {
+
+	public Vector2 ZoneForce;
+	public int layerToForce = 13; //layer of the player
+
+	private Rigidbody2D playerRigidbody;
+	private bool playerIsColliding = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void LateUpdate () {
+		if(playerIsColliding == true) {
+			Debug.Log ("AM COLLIDIN");
+			playerRigidbody.AddForce(ZoneForce);
+			playerIsColliding = false;
+		}
+	}
+
+	void OnTriggerStay2D(Collider2D other){
+		if(other.gameObject.layer == layerToForce){
+			if(playerRigidbody == null) playerRigidbody = other.rigidbody2D;
+			playerIsColliding = true;
+		}
+	}
+}
